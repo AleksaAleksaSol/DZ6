ee3188a6155c7208ef6c90c736b21e7f97e2d183
Aleksander <i@solovev-pro.ru>
2023-09-01 19:21:45 +0300

    Домашнее задание к семинару 6

---
+++ Task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.Clear();
+int CountOfElements(string message = "")
+{
+    Console.Write($"{message}Введите количество элементов в массиве:\t");
+    int elementsCount = int.Parse(Console.ReadLine());
+    return elementsCount;
+}
+int[] array = new int[CountOfElements()];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}:\t ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("\nСоздан массив:");
+Console.WriteLine();
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine();
+
+int CountPositivElements(int[] array)
+{
+    int countPositiv = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            countPositiv++;
+        }
+    }
+    return countPositiv;
+}
+Console.WriteLine($"В данном массиве количество чисел больше ноля равно: {CountPositivElements(array)} ");
+Console.WriteLine();
---
+++ Task46/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями y = k1 * x + b1, 
+// y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int SetNumbers(string message = " ")
+{
+    Console.Write($"{message}");
+    int variable = int.Parse(Console.ReadLine());
+    return variable;
+}
+Console.Clear();
+
+Console.WriteLine("Дано два уровнения прямых линий:  ");
+Console.WriteLine("\n1) y = k1 * x + b1");
+Console.WriteLine("2) y = k2 * x + b2");
+Console.WriteLine();
+
+double b1 = SetNumbers("Введите переменную b1: ");
+double k1 = SetNumbers("Введите переменную k1: ");
+double b2 = SetNumbers("Введите переменную b2: ");
+double k2 = SetNumbers("Введите переменную k2: ");
+
+//Решая систему уровнений получаем координаты точки пересечения х=(b2-b1)/(k1-k2); y=k1*((b2-b1)/(k1-k2))+b1
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*((b2-b1)/(k1-k2))+b1;
+Console.WriteLine($"\nКоординаты точки пересечения: ({x}, {y})");
+Console.WriteLine();
+Console.WriteLine();
